4dec8063ddaadfe0ade7e0c2b260847c4e2b7283
Reetahan Mukhopadhyay <reetahan@gmail.com>
2017-12-11 15:41:43 -0600

    Update timestuff.py
    n

--- timestuff.py
+++ timestuff.py
@@ -9,10 +9,12 @@ def timer(time, id):
 
     while(not(found_time)):
         now = datetime.datetime.now()
-        todays_hour = now.replace(hour=time, minute=0, second=0, microsecond=0)
-        if(now == todays_hour):
+        todays_hour = now.replace(hour=time, minute=0, second=0)
+        almost_hour = now.replace(hour=time,minute=0,second=1)
+        if(now == todays_hour) or (now == almost_hour):
             f = datastore.returnData(id)[1]
             return reddit.pick_sub(f)
+
 
 
 
